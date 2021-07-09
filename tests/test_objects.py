f55d617dbaee311c471212cd24c8b30e0a9aa5e6
Scott Phillips <polygonguru@gmail.com>
2021-07-09 13:04:18 -0400

    no message

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -2,7 +2,7 @@ import json
 from unittest import TestCase
 from lxml import etree
 
-from smartetailing.connection import SmartetailingConnection
+from smartetailing.connection import SmartetailingConnection, get_dollars
 from smartetailing.objects import WebOrder
 
 
@@ -31,6 +31,19 @@ class TestWebOrder(TestCase):
     def test_connect(self):
         connection = _get_connection()
         connection.export_orders_via_web()
+
+    def test_money_parse(self):
+        d1 = get_dollars("123")
+        d2 = get_dollars("2,345")
+        d3 = get_dollars("123.456")
+        d4 = get_dollars("001,234.567")
+        d5 = get_dollars("")
+
+        self.assertEqual(123.0, d1)
+        self.assertEqual(2345.0, d2)
+        self.assertEqual(123.456, d3)
+        self.assertEqual(1234.567, d4)
+        self.assertEqual(0.0, d5)
 
     def test_download_orders(self):
         connection = _get_connection()
